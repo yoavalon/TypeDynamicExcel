af5ad6cbfb9e64fdb8b25fb152525f53cb199f8b
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2017-08-24 13:33:27 +0300

    Added file

---
+++ DynamicExcel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type excelType = Type.GetTypeFromProgID("Excel.Application", true);
+            dynamic excel = Activator.CreateInstance(excelType);
+            excel.Workbooks.Add();
+
+            dynamic defaultWorksheet = excel.ActiveSheet;
+
+            defaultWorksheet.Cells[1, "A"] = "This is the column name";
+            defaultWorksheet.Columns[1].AutoFit();
+            defaultWorksheet.Name = "Test";
+
+            excel.Visible = true;
+
+        }
+    }
+}
